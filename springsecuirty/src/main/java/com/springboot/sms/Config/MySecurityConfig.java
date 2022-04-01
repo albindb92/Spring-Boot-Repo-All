4fcd97009976f44b9a38b3627aa49e8d68061c74
albindb92 <albindb92@users.noreply.github.com>
2022-04-02 01:30:10 +0530

    Customize multiple users in spring security 2

--- springsecuirty/src/main/java/com/springboot/sms/Config/MySecurityConfig.java
+++ springsecuirty/src/main/java/com/springboot/sms/Config/MySecurityConfig.java
@@ -1,10 +1,15 @@
 package com.springboot.sms.Config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 import com.fasterxml.jackson.databind.introspect.NopAnnotationIntrospector;
 
@@ -52,17 +57,31 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter{
 	}
 	
 	//Configure  multiple user for authentication - by default spring gives usernam as user and password can be taken from spring console.
+//	@Override
+//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//			
+//		auth.inMemoryAuthentication()
+//		.withUser("albin").password("12345").authorities("admin")
+//		.and()
+//		.withUser("user").password("12345").authorities("user")
+//		.and()
+//		.passwordEncoder(NoOpPasswordEncoder.getInstance());
+//		
+//	}
+//	
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-			
-		auth.inMemoryAuthentication()
-		.withUser("albin").password("12345").authorities("admin")
-		.and()
-		.withUser("user").password("12345").authorities("user")
-		.and()
-		.passwordEncoder(NoOpPasswordEncoder.getInstance());
-		
+		InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
+		UserDetails user1 = User.withUsername("albin").password("12345").authorities("admin").build();
+		UserDetails user2 = User.withUsername("user").password("12345").authorities("user").build();
+		userDetailsService.createUser(user1);
+		userDetailsService.createUser(user2);
+		auth.userDetailsService(userDetailsService);
 	}
 	
-	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
 }
