57e3068af77bb0e366d1ec80e304f1416ea0e8d4
albindb92 <albindb92@users.noreply.github.com>
2022-04-02 00:24:43 +0530

    Spring security customize security to protected few endppoints only

---
+++ springsecuirty/src/main/java/com/springboot/sms/HomeController/HomeController.java
@@ -0,0 +1,19 @@
+package com.springboot.sms.HomeController;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@GetMapping("/home")
+	public String home() {
+		return "home page";
+	}
+	
+	@GetMapping("/dashboard")
+	public String dashboard() {
+		return "This is dashboard";
+	}
+}
+
