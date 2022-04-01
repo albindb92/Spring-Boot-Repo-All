4a1fb6d34b86ca000ef842c2fbcd8f7ed443d717
albindb92 <albindb92@users.noreply.github.com>
2022-04-02 01:13:08 +0530

    Customize multiple users in spring security

--- springsecuirty/src/main/java/com/springboot/sms/Config/MySecurityConfig.java
+++ springsecuirty/src/main/java/com/springboot/sms/Config/MySecurityConfig.java
@@ -1,8 +1,12 @@
 package com.springboot.sms.Config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+import com.fasterxml.jackson.databind.introspect.NopAnnotationIntrospector;
 
 @Configuration
 public class MySecurityConfig extends WebSecurityConfigurerAdapter{
@@ -31,11 +35,34 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter{
 		// To deny all http request- EXAMPLE ADMIN ENDPOINTS CALLS SHOULD NOT BE ACCESSIBLE BY USER
 		
 		
+//		http
+//		.authorizeRequests()
+//		.antMatchers("/user/**").permitAll()
+//		.antMatchers("/admin/**").denyAll()
+//		.and()
+//		.httpBasic();
+		
+		
 		http
 		.authorizeRequests()
-		.antMatchers("/user/**").permitAll()
-		.antMatchers("/admin/**").denyAll()
+		.antMatchers("/user/home").permitAll()
+		.antMatchers("/user/dashboard").authenticated()
 		.and()
 		.httpBasic();
 	}
+	
+	//Configure  multiple user for authentication - by default spring gives usernam as user and password can be taken from spring console.
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+			
+		auth.inMemoryAuthentication()
+		.withUser("albin").password("12345").authorities("admin")
+		.and()
+		.withUser("user").password("12345").authorities("user")
+		.and()
+		.passwordEncoder(NoOpPasswordEncoder.getInstance());
+		
+	}
+	
+	
 }
